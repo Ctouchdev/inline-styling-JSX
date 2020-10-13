d0a497200a79df293874ced3fece7b23a45e88c6
Ctouchdev <63693926+Ctouchdev@users.noreply.github.com>
2020-10-13 17:15:16 -0500

    Updates

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const customStyle = {
+  color: "red",
+  fontSize: "2rem",
+  border: "2px solid red",
+  margin: "0",
+  textAlign: "center"
+};
+
+customStyle.border = "5px solid blue";
+
+ReactDOM.render(
+  <>
+    <h1 style={customStyle}>Hello World!</h1>
+    <h3 style={{ textAlign: "center", fontFamily: "verdana" }}>Hello</h3>
+  </>,
+  document.getElementById("root")
+);
